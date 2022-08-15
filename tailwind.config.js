c29a2c84c6ebbfc6ce993b2edccb17f89a86ebca
Derian Rosado <derian.rosado@alegra.com>
2022-08-14 20:00:18 -0500

    restart project

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-const smileConfig = require('@smile-ui/styles/tailwind.config')
-
-module.exports = {
-  content: [ './src/**/*.{vue,js,ts,jsx,tsx}' ],
-  theme: smileConfig.theme,
-  plugins: []
-}
