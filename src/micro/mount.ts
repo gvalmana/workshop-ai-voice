ffbd08d0c560441ba6144194eeb9470a1c779f22
Derian Rosado <derian.rosado@alegra.com>
2022-08-18 19:26:09 -0500

    Agregar styles tempalte

--- src/micro/mount.ts
+++ src/micro/mount.ts
@@ -1,5 +1,7 @@
 import routes from "./router/index";
 import { convertDictionaryToObject } from "./utils/appHelpers";
+import "../../node_modules/@alegradev/smile-ui-next/dist/style.css";
+import "../index.css";
 
 const microSettings = {
   routes,
