bef1b7a54025971ec065b7ff84820ccbf04b965e
Derian Rosado <derian.rosado@alegra.com>
2022-08-24 15:38:50 -0500

    fix error build testing, production

--- vue.config.js
+++ vue.config.js
@@ -8,9 +8,35 @@ const { MFLiveReloadPlugin } = require("@module-federation/fmr");
 const prefixEnvironment =
   process.env.VUE_APP_ENVIROMENT == "production" ? "" : "almost-";
 
+const plugins = [
+  new ModuleFederationPlugin({
+    name: "app_alegra_template",
+    filename: "remoteEntry.js",
+    remotes: {
+      app_alegra_commons: `app_alegra_commons@https://${prefixEnvironment}alegra-commons.alegra.com/remoteEntry.js`
+    },
+    exposes: {
+      "./microfront": "./src/micro/mount"
+    },
+    shared: {
+      ...dependencies
+    }
+  })
+];
+
+if (process.env.VUE_APP_ENVIROMENT == "local") {
+  plugins.push(
+    new MFLiveReloadPlugin({
+      port: 1026, // the port your app runs on
+      container: "app_alegra_template", // the name of your app, must be unique
+      standalone: false // false uses chrome extention
+    })
+  );
+}
+
 module.exports = defineConfig({
   assetsDir: "template-assets",
-  publicPath: "http://localhost:1026/",
+  publicPath: "http://dev.alegra.com:1026/",
   transpileDependencies: true,
   configureWebpack: {
     devServer: {
@@ -21,25 +47,6 @@ module.exports = defineConfig({
     optimization: {
       splitChunks: false
     },
-    plugins: [
-      new MFLiveReloadPlugin({
-        port: 1026, // the port your app runs on
-        container: "app_alegra_template", // the name of your app, must be unique
-        standalone: false // false uses chrome extention
-      }),
-      new ModuleFederationPlugin({
-        name: "app_alegra_template",
-        filename: "remoteEntry.js",
-        remotes: {
-          app_alegra_commons: `app_alegra_commons@https://${prefixEnvironment}alegra-commons.alegra.com/remoteEntry.js`
-        },
-        exposes: {
-          "./microfront": "./src/micro/mount"
-        },
-        shared: {
-          ...dependencies
-        }
-      })
-    ]
+    plugins
   }
 });
